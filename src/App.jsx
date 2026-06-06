1a470c59fbf9ef5a122caf10863e550f91d250c1
Ashish Ranjan <ash.ranjan09@gmail.com>
2026-06-06 08:18:22 +0530

    updated 'main 6' text to 'main 7' text to test and also added a lorem paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ import { Styled } from "./App.styled";
 const App = () => {
     return (
         <Styled.Wrapper>
-            <Styled.Main>main 6 test</Styled.Main>
+            <Styled.Main>
+                main 7 test
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Voluptatum totam, molestiae impedit incidunt expedita culpa
+                    amet autem non, ratione labore, earum vitae iure ipsam? Aut
+                    cupiditate repellendus reprehenderit nobis officiis?
+                </p>
+            </Styled.Main>
         </Styled.Wrapper>
     );
 };
