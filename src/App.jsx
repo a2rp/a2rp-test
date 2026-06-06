1ca986026af185b354db5ac486b9db8d7fc99b26
Ashish Ranjan <ash.ranjan09@gmail.com>
2026-06-06 08:03:29 +0530

    updated 'main 5' text to 'main 6' text to test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Styled } from "./App.styled";
 const App = () => {
     return (
         <Styled.Wrapper>
-            <Styled.Main>main 5 test</Styled.Main>
+            <Styled.Main>main 6 test</Styled.Main>
         </Styled.Wrapper>
     );
 };
