1d27f27aafbb52991a980f13ce243c64266b3bbd
Ashish Ranjan <ash.ranjan09@gmail.com>
2026-06-23 15:25:42 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
     return (
         <Styled.Wrapper>
             <Styled.Main>
-                <h1>18-06-2026 update</h1>
+                <h1>23-06-2026 updated</h1>
             </Styled.Main>
         </Styled.Wrapper>
     );
